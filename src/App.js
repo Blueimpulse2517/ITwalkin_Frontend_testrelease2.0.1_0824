c59abdfc0a3c1cc36c492561e04085201829e407
Blueimpulse2517 <136184059+Blueimpulse2517@users.noreply.github.com>
2025-06-26 10:17:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ import Footer from "./Job-Portal/Footer/Footer";
 
 import Payment from "./Job-Portal/Payment"
 //axios.defaults.baseURL = "http://localhost:8080"
-axios.defaults.baseURL = "https://itwalkin-backend-testrelease-2-0-1-0824.onrender.com";
+axios.defaults.baseURL = "https://itwalkin-backend-testrelease-2-0-1-0824-ns0g.onrender.com";
 const App = () => {
   return (
     <>
